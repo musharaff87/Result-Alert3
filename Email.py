96974f27e4d94793cdf23accf9f2ec7461545dae
Mohammed Musharaf Z <musharaff87@gmail.com>
2022-01-18 20:08:19 +0530

    commit

--- Email.py
+++ Email.py
@@ -2,7 +2,7 @@ import pandas as pd
 import smtplib
 
 SenderAddress = "musharafpes2021@gmail.com"
-password = "Sahidha@12"
+password = "pass123"
 
 e = pd.read_excel('Email.xlsx', engine='openpyxl')
 #e = pd.read_excel("Email.xlsx")
@@ -10,7 +10,9 @@ emails = e['Emails'].values
 server = smtplib.SMTP("smtp.gmail.com", 587)
 server.starttls()
 server.login(SenderAddress, password)
-msg = "Result arrived, Click the link to check results ..."
+msg = "Result arrived, Click the link to check results ..." \
+      "" \
+      "hello hi helloakchakcnakhas,sabcasijkasnfnnasbvkjshvioadhvaf"
 subject = "RESULT_ALERT!!!"
 body = "Subject: {}\n\n{}".format(subject,msg)
 for email in emails:
